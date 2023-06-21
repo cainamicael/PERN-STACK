89e137781e85527363fa214e302151f5d0c03651
cainamicael <cainamicaeloficial@gmail.com>
2023-06-20 21:49:20 -0300

    add

--- Course/TODO-LIST/server/index.js
+++ Course/TODO-LIST/server/index.js
@@ -6,11 +6,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-//Routes
 //List routes
 app.get('/todoes', async (req, res) => {
     try {
         const todoes = await pool.query(`SELECT * FROM todo`)
+
         res.status(200).json(todoes.rows)
     } catch (error) {
         console.error(error.message)
@@ -41,6 +41,21 @@ app.post('/todoes', async (req, res) => {
         console.error(error.message)
     }
 })
+
+//Update route
+app.put('/todoes/:id', async (req, res) => {
+    try {
+        const { description } = req.body
+        const { id } = req.params
+        const updateTodo = await pool.query(`UPDATE todo SET description = $1 WHERE todo_id = $2`, [description, id])
+
+        res.status(201).json({message: "Updated item!"})
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//Delete route
 
 app.listen(5000, () => {
     console.log('The server is runnind on 5000 port')
