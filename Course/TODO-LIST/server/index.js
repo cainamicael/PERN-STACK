b4964a5fdd8131aa808a8d59598e5082a0054e49
cainamicael <cainamicaeloficial@gmail.com>
2023-06-20 21:39:16 -0300

    add

--- Course/TODO-LIST/server/index.js
+++ Course/TODO-LIST/server/index.js
@@ -7,10 +7,35 @@ app.use(cors())
 app.use(express.json())
 
 //Routes
+//List routes
+app.get('/todoes', async (req, res) => {
+    try {
+        const todoes = await pool.query(`SELECT * FROM todo`)
+        res.status(200).json(todoes.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get('/todoes/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const todo = await pool.query(`SELECT * FROM todo WHERE todo_id = $1`, [id])
+
+        if(!todo.rows[0]) { return res.status(400).json({message: "Não encontrado!"}) }
+
+        res.status(200).json(todo.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//Insert route
 app.post('/todoes', async (req, res) => {
     try {
         const { description } = req.body
         const newTodo = await pool.query(`INSERT INTO todo (description) VALUES($1) RETURNING *`, [description]) //Avoid SQL Injections
+
         res.status(201).json(newTodo.rows[0])
     } catch (error) {
         console.error(error.message)
